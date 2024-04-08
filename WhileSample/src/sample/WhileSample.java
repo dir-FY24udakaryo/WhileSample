1ff9d2c19c1c4f3e3e20c055696e7168b41b7922
RyoUdaka(宇髙凌生) <RyoUdaka()@Dirbato-1401x>
2024-04-08 16:22:41 +0900

    演習終了

--- WhileSample/src/sample/WhileSample.java
+++ WhileSample/src/sample/WhileSample.java
@@ -3,8 +3,12 @@ package sample;
 public class WhileSample {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-
+        int num = 2;
+        
+        while (num <= 1024) {
+            System.out.println(num);
+            num = num * 2;
+        }
     }
 
 }
